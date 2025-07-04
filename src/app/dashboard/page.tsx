852eb4bf3f9bbd8d081904308ea1a9c5858b53dc
Mateus Cerqueira <mateus.cerq7@outlook.com>
2025-07-04 09:08:04 -0300

    feat: implement session management and redirection

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,22 @@
-const Dashboard = () => {
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import SignOutButton from "./components/sign-out-button";
+import { redirect } from "next/navigation";
+
+const Dashboard = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session?.user) {
+    redirect("/authentication");
+  }
   return (
-    <>
-      <div>
-        <h1>Dashboard Page</h1>
-      </div>
-    </>
+    <div>
+      <h1>Dashboard Page</h1>
+      <h1>{session?.user.name}</h1>
+      <h1>{session?.user.email}</h1>
+      <SignOutButton />
+    </div>
   );
 };
 
